12990cc6be422a987d4494fbd9ec942c4ba4bef9
concepcio.cozar <concepcio.cozar@stratesys-ts.com>
2022-04-26 10:13:16 +0200

    testing addBookToCart

--- src/app/services/book.service.spec.ts
+++ src/app/services/book.service.spec.ts
@@ -4,7 +4,7 @@ import { TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { Book } from '../models/book.model';
 import { environment } from '../../environments/environment.prod';
-import { Key } from 'protractor';
+import swal from 'sweetalert2';
 
 const listBook: Book[] = [
     {
@@ -29,6 +29,14 @@ const listBook: Book[] = [
         amount: 7
     }
 ];
+const book: Book =  {
+        name: '',
+        author: '',
+        isbn: '',
+        price: 15,
+        amount: 2
+};
+
 
 fdescribe('BookService', () => {
 
@@ -51,11 +59,17 @@ fdescribe('BookService', () => {
     beforeEach(() => {
         service = TestBed.inject(BookService);
         httpMock = TestBed.inject(HttpTestingController);
+        //reseteo del storage para que la información de un test no afecte al siguiente test
+        storage = {};
 
         //espia con un callFake para que no llame en realidad. Devuelve key si existe devuelve storage sino devuelve null
         spyOn(localStorage, 'getItem').and.callFake((key:string) => {
             return storage[key] ? storage[key] : null;
         })
+        //espia para el setItem que hace? primero pasa la key y luego un json
+        spyOn(localStorage, 'setItem').and.callFake((key: string, value: string) => {
+            return storage[key] = value;
+        });
     });
     //evita que no haya peticiones pendientes entre cada test. No lanza el siguiente test mientras haya una petición pendiente
     afterEach(() => {
@@ -96,6 +110,70 @@ fdescribe('BookService', () => {
         expect(listBook.length).toBe(0);
         
     });
+    /*public addBookToCart(book: Book) {
+    let listBook: Book[] = JSON.parse(localStorage.getItem('listCartBook'));
+    if (listBook === null) { // Create a list with the book
+      book.amount = 1;
+      listBook = [ book ];
+    } else { 
+      const index = listBook.findIndex((item: Book) => {
+        return book.id === item.id;
+      });
+      if (index !== -1) { // Update the quantity in the existing book
+        listBook[index].amount++;
+      } else { 
+        book.amount = 1;
+        listBook.push(book);
+      }
+    }
+    localStorage.setItem('listCartBook', JSON.stringify(listBook));
+    this._toastSuccess(book);
+  }*/
+  //método que comprueba antes de añadir al carrito si el libro existe en localStorage
+  //1o. testear caso: cuando la lista no existe añadir un libro
+    it('addBookToCart add a book when the list doesnt exist in the localStorage', () => {
+        //se crea un objeto con un método fire que devuelva null
+        const toast = {
+            fire: () => null
+        } as any;//as any para que no de error en el objeto toast
+        //el método mixin va a devolver un objeto toast que tiene un método fire que es null
+        const spy1 = spyOn(swal, 'mixin').and.callFake(() => {
+            return toast;
+        });
+        //el método fire no puede existir en null por lo que se ha de devolver un objeto que tenga como método .fire(mirar error)
+        //1º traemos la lista de libros
+        let listBook = service.getBooksFromCart();
+        //en este caso la lista tendria que ser 0
+        expect(listBook.length).toBe(0);
+        //enviar libro
+        
+        service.addBookToCart(book);
+        //como hemos añadido un libro en este caso ya tenemos 1 libro
+        listBook = service.getBooksFromCart();  
+        expect(listBook.length).toBe(1);
+        expect(spy1).toHaveBeenCalled();//llamamos al spy1 y se comprueba que haya sido llamado correctamente
+
+    });
+    it('addBookToCart add a book when the list exist in the localStorage', () => {
+   
+        const toast = {
+            fire: () => null
+        } as any;
+        const spy1 = spyOn(swal, 'mixin').and.callFake(() => {
+            return toast;
+        });
+        
+        let listBook = service.getBooksFromCart();
     
+        expect(listBook.length).toBe(0);
+        
+        service.addBookToCart(book);
+        listBook = service.getBooksFromCart();  
+        expect(listBook.length).toBe(1);
+        service.addBookToCart(book);
+        expect(spy1).toHaveBeenCalled();
+
+    });
+
 
 });
